f71162739252828763dbd5ea977e4648e772ccbd
suu-otl <66276382+suu-otl@users.noreply.github.com>
2021-05-20 14:50:00 -0600

    Add files via upload

--- test.js
+++ test.js
@@ -5,12 +5,16 @@ var classDictionary = {
 	"MATH" : ["https://www.suu.edu/academicsuccess/tutoring/", "Tutoring"],
 	"COMM" : ["https://www.suu.edu/speech-center/", "Speech and Presentation Assistance"]
 }
-console.log("HELLO WOORLD")
-print("HELLO WORLD")
+
+console.log("Hello world");
 
 if(window.location.pathname.includes("courses")){
 	var title = document.title.split("-")[0];
 	if(classDictionary[title]){
 		$('#course_show_secondary').append(`<br /> <a class="Button Button--primary Button--block" href=${classDictionary[title][0]} >${classDictionary[title][1]}</a> `);
 	}
+}
+
+if(window.location.pathname.includes("speed_grader")){
+	$(right_side).append('<a class="Button Button--primary Button--block" href="Google.com">Plz work</a> ');
 }
