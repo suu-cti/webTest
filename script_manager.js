333634a6d048c981291ce295738b1b4f533efc8f
suu-otl <66276382+suu-otl@users.noreply.github.com>
2021-05-21 11:51:43 -0600

    Add files via upload

--- script_manager.js
+++ script_manager.js
@@ -3,7 +3,7 @@ console.log("Script manager working")
 var scripts = ['third_party_scripts.js', 'buttons.js']
 for(var i = 0; i < scripts.length; i++){
 	var script = document.createElement("script");
-	script.src = 'https://cdn.jsdelivr.net/gh/suu-otl/webTest@3a11a185be558a637d56c54bd301d80a0b899c96/' + scripts[i];
+	script.src = 'https://cdn.jsdelivr.net/gh/suu-otl/webTest@f065e4dab925545dea84a04d9dbd9ec345e0b88f/' + scripts[i];
 	document.head.appendChild(script);
 }
 
