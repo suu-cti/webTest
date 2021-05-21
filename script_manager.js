3a11a185be558a637d56c54bd301d80a0b899c96
suu-otl <66276382+suu-otl@users.noreply.github.com>
2021-05-21 09:52:13 -0600

    Add files via upload

---
+++ script_manager.js
@@ -0,0 +1,7 @@
+//Calls all of the other scripts
+//LINKTO: buttons, 
+
+console.log("HELLLLLLLLLLLOOO WORLD!")
+if(window.location.pathname.includes("speed_grader")){
+	$(right_side).append('<a class="Button Button--primary Button--block" href="Google.com">Plz work</a> ');
+}
